192220644ab646b2f0aa52b724288ad026fda64b
Edson1991 <edsonhenrique.lima@yahoo.com.br>
2020-12-04 17:56:55 -0300

    Comitando Sozinho no GITHUB

---
+++ Hello/src/HelloEclipse.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class HelloEclipse{
+    
+    public static void main(String args[]){
+        Scanner teclado = new Scanner(System.in);
+        int idade=0;
+        int cont=0;
+        double soma=0;
+       
+        while (true){
+        	System.out.println("Digite um valor inteiro");
+            idade = teclado.nextInt();
+            if (idade < 0){
+                break;
+            }
+            soma = soma + idade;
+            cont++;            
+        }
+        System.out.printf("A média dos valores digitados é: %.2f%n",soma/cont);
+        teclado.close();
+    }
+}
+
